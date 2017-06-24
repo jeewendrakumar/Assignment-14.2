b1c8b44e7642b5ad4333cabfb806f59758c722bc
jeewendra kumar <jeewendrakumar@gmail.com>
2017-06-25 03:27:21 +0530

    Add files via upload

---
+++ app/js/components/Book.js
@@ -0,0 +1,34 @@
+import React from "react";
+import Relay from "react-relay";
+
+class Book extends React.Component {
+
+    render() {
+        const {book, index} = this.props;
+        const {firstName, lastName} = book.author;
+        return (
+            <tr>
+                <td>{index}</td>
+                <td>{book.title}</td>
+                <td>{firstName} {lastName}</td>
+                <td>
+                    <button type="button" className="btn btn-danger" onClick={this.handleBookDelete}>Delete</button>
+                </td>
+            </tr>
+        );
+    }
+}
+
+export default Relay.createContainer(Book, {
+    fragments: {
+        book: () => Relay.QL `
+        fragment book on Book {
+            title,
+            author{
+                firstName
+                lastName
+            }
+        }
+        `
+    }
+});
